13e6fbae0569c8dc053f932eefd60e307a588526
limengyi <1051933474@qq.com>
2021-06-16 16:00:43 +0800

    chore: 构建基本项目环境

---
+++ src/router/router.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+
+const router = new Router({
+    mode: 'history', //去除url中的#号
+    routes: [
+        {
+            path: '/',
+            component: r => require.ensure([], () => r(require('../pages/index.vue')), 'index'),
+            meta: {
+                title: 'index'
+            }
+        },
+    ]
+})
+
+router.beforeEach((to, from, next) => {
+    /* 路由发生变化修改页面title */
+    if (to.meta.title) {
+      document.title = to.meta.title
+    }
+    next()
+});
+  
+export default router
---
+++ webpack.dev.js
@@ -0,0 +1,1 @@
+module.exports = require('webpack-project-builder/lib/webpack.single')
