8ee9bc267d1162265fb0b43ea991789c9981bfbe
limengyi <1051933474@qq.com>
2021-06-18 14:59:52 +0800

    chore: 项目运行命令添加运行环境判断

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import vue from 'vue';
+import Vue from 'vue';
 import Editor from './editor/index.vue';
 
 import './assets/normalize.css';
 
-new vue({
-    el: '#lightEditor',
-    render: h => h(Editor)
-});
+if (process.env.NODE_ENV === 'development') {
+    new Vue({
+        el: '#lightEditor',
+        render: h => h(Editor)
+    });
+}
 
-Editor.install = Vue => {
-    if (!Vue) {
-        window.Vue = vue;
-    }
-    Vue.component(Editor.name, Editor);
-};
 export default Editor;
